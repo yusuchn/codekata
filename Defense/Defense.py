9101317dc7c4deaa90a483a13b62cd4e2da69c01
Yu Su <ysu1@illumina.com>
2019-08-23 08:48:31 +0100

    minor modification to Defense

--- Defense/Defense.py
+++ Defense/Defense.py
@@ -1,40 +1,41 @@
 from generaltools import *
-import string
-import copy
 import pprint
 from PIL import Image
 
 
-score_dict = {'F': '10', 'W': 0, 'M': 100, "G": 1}
+score_dict = {'F': '10', 'W': 0, 'M': 100, "G": 1, "V": -1}
 villege_letter = 'V'
 villege_rgb = (54, 61, 0)
 
 
 def load_from_image(map_jpag_filename):
     try:
-        im_rgb_list = list()
         im = Image.open(map_jpag_filename)    # (map_jpag_filename)  # Can be many different formats.
         print('image size = {}'.format(im.size))  # Get the width and hight of the image for iterating over
-        pix = im.load()     # pixel RGB value of the image
-        for i in range(im.size[0]):
-            row_rgb_list = list()
-            for j in range(im.size[1]):
-                row_rgb_list.append(pix[i, j])
-                # if pix[i, j] == (0, 67, 154):
-                #     pix[i, j] = (0, 0, 0)     # modify pixel rgb value
-            im_rgb_list.append(row_rgb_list)
-        # im.save('modified_im.png')  # Save the modified pixels as .png
-        return True, im_rgb_list
+        rgb_list = get_rgb_list(im)
+        return True, rgb_list
     except:
         print("No image map file exists")
         return False, None
 
 
+def get_rgb_list(im_param):
+    pix = im_param.load()  # pixel RGB value of the image
+    rgb_list = [[(0, 0, 0)] * im_param.size[1] for n in range(im_param.size[0])]
+    for i in range(im_param.size[0]):
+        for j in range(im_param.size[1]):
+            rgb_list[i][j] = pix[i, j]
+            # if pix[i, j] == (0, 67, 154):
+            #     pix[i, j] = (0, 0, 0)     # modify pixel rgb value
+    # im.save('modified_im.png')  # Save the modified pixels as .png
+    return rgb_list
+
+
 def load_from_text(map_txt_filename):
     try:
         lines = open(map_txt_filename)     # (map_txt_filename)    #
-        map_letter_list = get_letter_list(lines)
-        return True, map_letter_list
+        letter_list = get_letter_list(lines)
+        return True, letter_list
     except:
         print("No text map file exists")
         return False, None
@@ -42,45 +43,64 @@ def load_from_text(map_txt_filename):
 
 def get_letter_list(lines_param):
     letter_list = list()
-    for j, line in enumerate(lines_param):
+    for i, line in enumerate(lines_param):
         line_letter_list = list()
-        for letter in line.strip():
+        for j, letter in enumerate(line.strip()):
             line_letter_list.append(letter)
         letter_list.append(line_letter_list)
     return letter_list
 
 
-def find_village_from_letter_map(letter_map_param):
-    total_row = len(letter_map_param)
-    total_col = len(letter_map_param[0])
-    for i in range(total_row):
-        for j in range(total_col):
-            if letter_map_param[i][j] == villege_letter:        # 'V':
-                return (i, j), total_row, total_col
-
-
-def find_village_from_rgb_map(rgb_map_param):
+def find_village_from_letter_list(letter_list_param):
     global villege_letter
-    total_row = len(rgb_map_param)
-    total_col = len(rgb_map_param[0])
+    total_row = len(letter_list_param)
+    total_col = len(letter_list_param[0])
     for i in range(total_row):
         for j in range(total_col):
-            if rgb_map_param[i][j] == villege_rgb:
+            if letter_list_param[i][j] == villege_letter:        # 'V':
                 return (i, j), total_row, total_col
 
 
-loaded, text_loaded_map = load_from_text('map.txt')
-nparray_text_loaded_map = np.array(text_loaded_map)
-v, total_row, total_col = find_village_from_letter_map(text_loaded_map)
-print('v={}, total_row={}, total_col={}'.format(v, total_row, total_col))
-# pprint.pprint('text_loaded_map=\n{}'.format(text_loaded_map))
-print('nparray_text_loaded_map=\n{}'.format(nparray_text_loaded_map))
+def find_village_from_rgb_list(rgb_list_param):
+    global villege_rgb
+    total_row = len(rgb_list_param)
+    total_col = len(rgb_list_param[0])
+    for i in range(total_row):
+        for j in range(total_col):
+            if rgb_list_param[i][j] == villege_rgb:
+                return (i, j), total_row, total_col
 
-loaded_im, rgb_loaded_map = load_from_image('Defense.jpg')
-nparray_rgb_loaded_map = np.array(rgb_loaded_map)
-v, total_row, total_col = find_village_from_rgb_map(rgb_loaded_map)
+
+def build_cost_list(letter_list_param):
+    global score_dict       # = {'F': '10', 'W': 0, 'M': 100, "G": 1, "V": -1}
+    total_row = len(letter_list_param)
+    total_col = len(letter_list_param[0])
+    cost_list = [[0] * total_col for n in range(total_row)]
+    for i in range(total_row):
+        for j in range(total_col):
+            cost_list[i][j] = score_dict[letter_list_param[i][j]]
+    return cost_list
+
+
+loaded, letter_list = load_from_text('map.txt')
+nparray_letter_list = np.array(letter_list)
+v, total_row, total_col = find_village_from_letter_list(letter_list)
 print('v={}, total_row={}, total_col={}'.format(v, total_row, total_col))
-# pprint.pprint('rgb_loaded_map = {}'.format(rgb_loaded_map))
-print('nparray_rgb_loaded_map=\n{}'.format(nparray_rgb_loaded_map))
+# pprint.pprint('letter_list=\n{}'.format(letter_list))
+print('nparray_letter_list=\n{}'.format(nparray_letter_list))
+
+loaded_im, rgb_list = load_from_image('Defense.jpg')
+nparray_rgb_list = np.array(rgb_list)
+v, total_row, total_col = find_village_from_rgb_list(rgb_list)
+print('v={}, total_row={}, total_col={}'.format(v, total_row, total_col))
+# pprint.pprint('rgb_list = {}'.format(rgb_list))
+print('nparray_rgb_list=\n{}'.format(nparray_rgb_list))
+
+cost_list = build_cost_list(letter_list)
+nparray_cost_list = np.array(cost_list)
+# pprint.pprint('cost_list = {}'.format(cost_list))
+print('nparray_cost_list=\n{}'.format(nparray_cost_list))
+
+
 
 
