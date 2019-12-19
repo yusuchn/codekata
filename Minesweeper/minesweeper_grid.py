19341d68b4f17113293d0c354e288958070a37b8
ILLUMINA\ysu1 <ysu1@illumina.com>
2019-12-19 08:58:15 +0000

    latest dev

--- Minesweeper/minesweeper_grid.py
+++ Minesweeper/minesweeper_grid.py
@@ -24,7 +24,6 @@ def on_left_mouse_click(event, grid_arg, grid_text_arg, canvas_arg, text_shift_a
 
 
 def on_right_mouse_click(event, grid_arg, grid_text_arg, canvas_arg, text_shift_arg):
-    # todo, give up anyway, restart the game
     if messagebox.askyesno("Game", 'Restart?'):
         reset_grid_text('?', grid_text_arg)
         draw_grid_use_block(canvas_arg, grid_arg, grid_text_arg, text_shift_arg)
@@ -80,27 +79,6 @@ def main():
                            grid_text_arg=grid_text,
                            canvas_arg=canvas, text_shift_arg=text_shift:
                     on_right_mouse_click(event, grid_arg, grid_text_arg, canvas_arg, text_shift_arg))
-
-        # fixed_number_letters = get_fixed_number_letters(letters)
-        #
-        # board_floating_number_letters = get_board_floating_number_letters(board, fixed_number_letters)
-        # original_letter_board = generate_letter_board(board, board_floating_number_letters, fixed_number_letters)
-        # nparray_board = np.array(board)
-        # nparray_original_letter_board = np.array(original_letter_board)
-        #
-        # print('puzzle = \n{}'.format(puzzle))
-        # print('fixed_number_letters = {}'.format(fixed_number_letters))
-        # pprint.pprint('board = \n{}'.format(board))
-        # print('nparray_board = \n{}'.format(nparray_board))
-        # print('nparray_original_letter_board = \n{}'.format(nparray_original_letter_board))
-        #
-        # solved, new_fixed_number_letters, solved_letter_board = solve_puzzle(0, board, fixed_number_letters, dictionary)
-        # if solved:
-        #     # for some reason, the returning value of new_fixed_number_letters doesn't give the correct value
-        #     # uncomment the following line to see the phenomena
-        #     # pprint.pprint('fixed_number_letters = \n{}'.format(new_fixed_number_letters))
-        #     nparray_solved_letter_board = np.array(solved_letter_board)
-        #     print('nparray_solved_letter_board = \n{}'.format(nparray_solved_letter_board))
 
     root.mainloop()
 
